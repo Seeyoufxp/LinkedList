72f8de2f9baec182b23b2cd50bb28bb355059670
Seeyoufxp <seeyoufxp@gamil.com>
2024-10-09 16:57:18 +0800

    带哨兵

---
+++ DoubleLinkedList.java
@@ -0,0 +1,113 @@
+package love.seeyoufxp.doublelinkedlist;
+
+import java.util.Iterator;
+
+public class DoubleLinkedList implements Iterable<Integer> {
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            Node p = head.next;
+
+            @Override
+            public boolean hasNext() {
+                return p != tail;
+            }
+
+            @Override
+            public Integer next() {
+                int value = p.value;
+                p = p.next;
+                return value;
+            }
+        };
+    }
+
+    static class Node {
+        Node prev;
+        int value;
+        Node next;
+
+        public Node(Node prev, int value, Node next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    public DoubleLinkedList() {
+        head = new Node(null, 666, null);
+        tail = new Node(null, 888, null);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public void addLast(int value) {
+        Node last = tail.prev;
+        Node added = new Node(last, value, tail);
+        last.next = added;
+        tail.prev = added;
+    }
+
+    public void removeLast() throws IllegalAccessException {
+        Node removed = tail.prev;
+        if (removed == head) {
+            throw illegalIndex(0);
+        }
+        Node prev = removed.prev;
+        prev.next = tail;
+        tail.prev = prev;
+    }
+
+    private Node findNode(int index) {
+        int i = -1;
+        for (Node p = head; p != tail; p = p.next, i++) {
+            if (i == index) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void addFirst(int value) throws IllegalAccessException {
+        insert(0, value);
+    }
+
+    public void insert(int index, int value) throws IllegalAccessException {
+        Node prev = findNode(index - 1);
+        if (prev == null) {
+            throw illegalIndex(index);
+        }
+        Node next = prev.next;
+        Node inserted = new Node(prev, value, next);
+        prev.next = inserted;
+        next.prev = inserted;
+    }
+
+    public void removeFirst() throws IllegalAccessException {
+        remove(0);
+    }
+
+    public void remove(int index) throws IllegalAccessException {
+        Node prev = findNode(index - 1);
+        if (prev == null) {
+            throw illegalIndex(index);
+        }
+        Node removed = prev.next;
+        if (removed == tail) {
+            throw illegalIndex(index);
+        }
+        Node next = removed.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private static IllegalAccessException illegalIndex(int index) {
+        return new IllegalAccessException(
+                String.format("index [%d] 不合法", index));
+    }
+
+
+}
