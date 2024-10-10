c273f080e27d6fc359d0762901c98b8b20f449a7
Seeyoufxp <seeyoufxp@gamil.com>
2024-10-10 11:12:47 +0800

    带哨兵

---
+++ CircularLinkedList.java
@@ -0,0 +1,60 @@
+package love.seeyoufxp.circularlinkedlist;
+
+import java.util.Iterator;
+
+public class CircularLinkedList implements Iterable<Integer> {
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            Node p = sentinel.next;
+
+            @Override
+            public boolean hasNext() {
+                return p != sentinel;
+            }
+
+            @Override
+            public Integer next() {
+                int value = p.value;
+                p = p.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node {
+        Node prev;
+        int value;
+        Node next;
+
+        public Node(Node prev, int value, Node next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node sentinel = new Node(null, -1, null);
+
+    public CircularLinkedList() {
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    public void addFirst(int value) {
+        Node a = sentinel;
+        Node b = sentinel.next;
+        Node added = new Node(a, value, b);
+        a.next = added;
+        b.prev = added;
+    }
+
+    public void addLast(int value) {
+        Node a = sentinel.prev;
+        Node b = sentinel;
+        Node added = new Node(a, value, b);
+        a.next = added;
+        b.prev = added;
+    }
+}
